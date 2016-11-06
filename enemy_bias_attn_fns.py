9ab59e90cf507d89404e44b655a55c06fe3ed59f
Keeley Erhardt <kerhardt@circle.com>
2016-11-06 17:25:37 -0500

    Initial commit

---
+++ enemy_bias_attn_fns.py
@@ -0,0 +1,42 @@
+def attention_numerical_toward_combatant(combatant_name):
+    """Calculates a score representing the attention paid towards a given combatant
+        Args:
+            combatant_name(str): the name of the combatant
+        Return:
+            attentiveness_score(float): a score of how attentive the user is
+    """
+    return attentiveness_score
+
+
+def attention_toward_combatant(combatant_name,
+                               attentiveness_score,
+                               neutral_attn_threshold=.4,
+                               high_attn_threshold=.7):
+    """Calculates the general attentiveness of a user towards a given combatant
+        Args:
+            combatant_name(str): the name of the combatant
+        Return:
+            a general attentiion score from low, neutral and high
+    """
+    if attentiveness_score < neutral_attn_threshold:
+        return "low_attention"
+    elif attentiveness_score < high_attn_threshold:
+        return "neutral_attention"
+    return "high_attention"
+
+
+def attention_bool_toward_combatant(combatant_name,
+                                    attentiveness_score,
+                                    is_attentive_threshold=.5):
+    """Determines if a user is attentive towards a given combatant
+        Args:
+            combatant_name(str): the name of the combatant
+            attentiveness_score(float): a numberical representation of a
+                                        user's attentiveness
+            is_attentive_threshold(float): the threshold value to be considered attentive
+        Return:
+            true if user is attentive else false
+    """
+    if attentiveness_score < is_attentive_threshold:
+        return False
+    return True
